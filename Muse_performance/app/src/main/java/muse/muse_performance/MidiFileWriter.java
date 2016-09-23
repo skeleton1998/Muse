e7494e6fedb7a2cb94fa67943b00fe68afa0389e
Shun01a4 <ssukee.999@gmail.com>
2016-09-23 21:32:40 +0900

    本番用プロジェクト
    ファイル作成のみ

---
+++ Muse_performance/app/src/main/java/muse/muse_performance/MidiFileWriter.java
@@ -0,0 +1,7 @@
+package muse.muse_performance;
+
+/**
+ * Created by Shun on 2016/09/23.
+ */
+public class MidiFileWriter {
+}
---
+++ Muse_performance/app/src/main/java/muse/muse_performance/Starting.java
@@ -0,0 +1,25 @@
+package muse.muse_performance;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Starting extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_starting);
+
+		Button sendbutton1=(Button)findViewById(R.id.send_button);//MainMenu遷移のボタン
+		sendbutton1.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				Intent intent1=new Intent(getApplication(),MainMenu.class);
+				startActivity(intent1);
+			}
+		});
+	}
+}
