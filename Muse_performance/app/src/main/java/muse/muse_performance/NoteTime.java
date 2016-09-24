2ca80696e74663ae8b9ee1b7a9e5803fff115f9c
ryo628 <ryokirito628@gmail.com>
2016-09-24 14:09:54 +0900

    2曲目追加
    ファイル分割

---
+++ Muse_performance/app/src/main/java/muse/muse_performance/NoteTime.java
@@ -0,0 +1,16 @@
+package muse.muse_performance;
+
+/**
+ * Created by Ryo on 2016/09/24.
+ */
+public class NoteTime
+{
+	public final static double Note_010 = 4;    // 全音符
+	public final static double Note_025 = 3;    // 付点2分音符
+	public final static double Note_020 = 2;    // 2分音符
+	public final static double Note_045 = 1.5;  // 付点4分音符
+	public final static double Note_040 = 1;    // 4分音符
+	public final static double Note_085 = 0.75; // 付点8分音符
+	public final static double Note_080 = 0.5;  // 8分音符
+	public final static double Note_160 = 0.25; // 16分音符
+}
